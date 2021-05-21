91717dff95b922dca757458f18b0b7a59fcb9251
Alfredo Martínez <alfre_canos13@icloud.com>
2021-05-21 23:09:10 +0200

    new website with Gatsby. Nav bar create

---
+++ src/pages/order.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function OrderPage() {
+  return (
+    <div>
+      <p>OrderPage</p>
+    </div>
+  );
+}
---
+++ src/pages/slicemasters.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Nav from '../components/Nav';
+
+export default function SlicemastersPage() {
+  return (
+    <div>
+      <Nav />
+      <p>SlicemastersPage</p>
+    </div>
+  );
+}
