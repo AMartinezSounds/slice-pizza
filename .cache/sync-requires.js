aef73cda613db2ae1f55b5cb64399280115f1e13
Alfredo Martínez <alfre_canos13@icloud.com>
2021-05-29 23:03:22 +0200

    pagination on slicemasters ready

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -1,17 +1,17 @@
-const { hot } = require("react-hot-loader/root")
+
 
 // prefer default export if available
 const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/a.martinez/Desktop/gatsby-projects/slices/gatsby/.cache/dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("/Users/a.martinez/Desktop/gatsby-projects/slices/gatsby/src/pages/404.js"))),
-  "component---src-pages-beers-js": hot(preferDefault(require("/Users/a.martinez/Desktop/gatsby-projects/slices/gatsby/src/pages/beers.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/a.martinez/Desktop/gatsby-projects/slices/gatsby/src/pages/index.js"))),
-  "component---src-pages-order-js": hot(preferDefault(require("/Users/a.martinez/Desktop/gatsby-projects/slices/gatsby/src/pages/order.js"))),
-  "component---src-pages-pizzas-js": hot(preferDefault(require("/Users/a.martinez/Desktop/gatsby-projects/slices/gatsby/src/pages/pizzas.js"))),
-  "component---src-pages-slicemasters-js": hot(preferDefault(require("/Users/a.martinez/Desktop/gatsby-projects/slices/gatsby/src/pages/slicemasters.js"))),
-  "component---src-templates-pizza-js": hot(preferDefault(require("/Users/a.martinez/Desktop/gatsby-projects/slices/gatsby/src/templates/Pizza.js")))
+  "component---cache-dev-404-page-js": (preferDefault(require("/Users/a.martinez/Desktop/gatsby-projects/slices/gatsby/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": (preferDefault(require("/Users/a.martinez/Desktop/gatsby-projects/slices/gatsby/src/pages/404.js"))),
+  "component---src-pages-beers-js": (preferDefault(require("/Users/a.martinez/Desktop/gatsby-projects/slices/gatsby/src/pages/beers.js"))),
+  "component---src-pages-index-js": (preferDefault(require("/Users/a.martinez/Desktop/gatsby-projects/slices/gatsby/src/pages/index.js"))),
+  "component---src-pages-order-js": (preferDefault(require("/Users/a.martinez/Desktop/gatsby-projects/slices/gatsby/src/pages/order.js"))),
+  "component---src-pages-pizzas-js": (preferDefault(require("/Users/a.martinez/Desktop/gatsby-projects/slices/gatsby/src/pages/pizzas.js"))),
+  "component---src-pages-slicemasters-js": (preferDefault(require("/Users/a.martinez/Desktop/gatsby-projects/slices/gatsby/src/pages/slicemasters.js"))),
+  "component---src-templates-pizza-js": (preferDefault(require("/Users/a.martinez/Desktop/gatsby-projects/slices/gatsby/src/templates/Pizza.js")))
 }
 
--- gatsby-node.js
+++ gatsby-node.js
@@ -75,6 +75,37 @@ async function fetchBeersAndTurnIntoNodes({
   }
 }
 
+async function turnSlicemastersIntoPages({ graphql, actions }) {
+  const { data } = await graphql(`
+    query {
+      slicemasters: allSanityPerson {
+        totalCount
+        nodes {
+          name
+          id
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `);
+  const pageSize = parseInt(process.env.GATSBY_PAGE_SIZE);
+  const pageCount = Math.ceil(data.slicemasters.totalCount / pageSize);
+  Array.from({ length: pageCount }).forEach((_, i) => {
+    console.log(`CREATING PAGE ${i}`);
+    actions.createPage({
+      path: `/slicemasters/${i + 1}`,
+      component: path.resolve('./src/pages/slicemasters.js'),
+      context: {
+        skip: i * pageSize,
+        currentPage: i + 1,
+        pageSize,
+      },
+    });
+  });
+}
+
 export async function sourceNodes(params) {
   await Promise.all([fetchBeersAndTurnIntoNodes(params)]);
 }
@@ -83,5 +114,6 @@ export async function createPages(params) {
   await Promise.all([
     turnPizzasIntoPages(params),
     turnToppingsIntoPages(params),
+    turnSlicemastersIntoPages(params),
   ]);
 }
