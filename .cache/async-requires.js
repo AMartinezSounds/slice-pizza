0e96624bdd75b9b1b1e478e5aabc9ec07b020971
Alfredo Martínez <alfre_canos13@icloud.com>
2021-05-31 09:43:59 +0200

    build

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,9 +2,7 @@
 const preferDefault = m => (m && m.default) || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-beers-js": () => import("./../../../src/pages/beers.js" /* webpackChunkName: "component---src-pages-beers-js" */),
   "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
   "component---src-pages-order-js": () => import("./../../../src/pages/order.js" /* webpackChunkName: "component---src-pages-order-js" */),
   "component---src-pages-pizzas-js": () => import("./../../../src/pages/pizzas.js" /* webpackChunkName: "component---src-pages-pizzas-js" */),
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,9 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": (preferDefault(require("/Users/a.martinez/Desktop/gatsby-projects/slices/gatsby/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": (preferDefault(require("/Users/a.martinez/Desktop/gatsby-projects/slices/gatsby/src/pages/404.js"))),
-  "component---src-pages-beers-js": (preferDefault(require("/Users/a.martinez/Desktop/gatsby-projects/slices/gatsby/src/pages/beers.js"))),
   "component---src-pages-index-js": (preferDefault(require("/Users/a.martinez/Desktop/gatsby-projects/slices/gatsby/src/pages/index.js"))),
   "component---src-pages-order-js": (preferDefault(require("/Users/a.martinez/Desktop/gatsby-projects/slices/gatsby/src/pages/order.js"))),
   "component---src-pages-pizzas-js": (preferDefault(require("/Users/a.martinez/Desktop/gatsby-projects/slices/gatsby/src/pages/pizzas.js"))),
